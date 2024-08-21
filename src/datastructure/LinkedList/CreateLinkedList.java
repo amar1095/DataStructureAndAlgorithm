2576b79564d556f1066a87ae661faedb2af87fe4
Amarnath Singh <amarbaba1995@gmail.com>
2024-08-22 00:52:59 +0530

    insert node in LinkeList

--- src/datastructure/LinkedList/CreateLinkedList.java
+++ src/datastructure/LinkedList/CreateLinkedList.java
@@ -1,17 +1,35 @@
 package datastructure.LinkedList;
 
-
 public class CreateLinkedList {
 	public static void main(String[] args) {
-		
-		LinkedList list=new LinkedList();
+
+		LinkedList list = new LinkedList();
+
+		// create a linkedlist
 		list.appendNode(3);
 		list.appendNode(4);
 		list.appendNode(5);
 		list.appendNode(8);
 		list.printLinkedList();
 
-		
+		// insert new node at begining
+		list.insertNewNodeAtBegin(7);
+		System.out.println();
+		System.out.println("after insert new Node at begin");
+		list.printLinkedList();
+
+		// insert new node at end
+		list.insertNewNodeAtEnd(12);
+		System.out.println();
+		System.out.println("after inser new Node at end");
+		list.printLinkedList();
+
+		// insert new node at middle
+		list.insertNewNodeatMiddle(14);
+		System.out.println();
+		System.out.println("after insert new Node at middle");
+		list.printLinkedList();
+
 	}
 
 }
